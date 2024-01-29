5713259b2f4114ce219458bbc5d24ad2d06f4b7d
Jesse Kelly <kellyjesse881@gmail.com>
2024-01-29 18:53:18 +0100

    chore: refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 pub struct CPU {
     pub register_a: u8,
+    pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
 }
@@ -9,8 +10,35 @@ impl CPU {
     pub fn new() -> Self {
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
             program_counter: 0,
+        }
+    }
+
+    fn lda(&mut self, value: u8) {
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a)
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x)
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        // is zero?
+        if result == 0 {
+            self.status = self.status | 0b0000_0010; // set zero flag
+        } else {
+            self.status = self.status & 0b1111_1101; // unset zero flag
+        }
+
+        // is negative?
+        if result & 0b1000_0000 != 0 {
+            self.status = self.status | 0b1000_0000; // set negative flag
+        } else {
+            self.status = self.status & 0b0111_1111; // unset negative flag
         }
     }
 
@@ -29,22 +57,10 @@ impl CPU {
                 0xA9 => {
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1; // pass over param
-                    self.register_a = param;
-
-                    // is zero?
-                    if self.register_a == 0 {
-                        self.status = self.status | 0b0000_0010; // set zero flag
-                    } else {
-                        self.status = self.status & 0b1111_1101; // unset zero flag
-                    }
-
-                    // is negative?
-                    if self.register_a & 0b1000_0000 != 0 {
-                        self.status = self.status | 0b1000_0000; // set negative flag
-                    } else {
-                        self.status = self.status & 0b0111_1111; // unset negative flag
-                    }
+                    self.lda(param)
                 }
+
+                0xAA => self.tax(),
 
                 _ => todo!(""),
             }
@@ -57,7 +73,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_0xa9_load() {
+    fn test_0xa9_lda_load() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x05, 0x00]); // load 5; break;
 
@@ -67,10 +83,19 @@ mod test {
     }
 
     #[test]
-    fn test_0xa9_zero_flag() {
+    fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x00, 0x00]); // load 5; break;
         assert!(cpu.status & 0b0000_0010 == 0b10); // zero flag set
+    }
+
+    #[test]
+    fn test_0xaa_tax() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 10;
+        cpu.interpret(vec![0xaa, 0x00]); // tax; break;
+
+        assert_eq!(cpu.register_x, 10)
     }
 }
 
